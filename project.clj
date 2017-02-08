76d4689fa2246248dc2d7cf0d9a87215964604e4
Jason Bell <jason@mastodonc.com>
2017-02-08 08:46:07 +0000

    Basic Onyx Template Setup

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject kixi.hecuba.onyx.smartmeter "0.1.0-SNAPSHOT"
+  :description ""
+  :url ""
+  :license {:name ""
+            :url ""}
+  :dependencies [[aero "1.0.0-beta2"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [org.onyxplatform/onyx "0.9.10"]
+                 [org.onyxplatform/lib-onyx "0.9.10.0"]]
+  :source-paths ["src"]
+
+  :profiles {:dev {:jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
+                   :global-vars {*assert* true}
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                                  [lein-project-version "0.1.0"]]}
+
+             :uberjar {:aot [lib-onyx.media-driver
+                             kixi.hecuba.onyx.smartmeter.core]
+                       :uberjar-name "peer.jar"
+                       :global-vars {*assert* false}}})
